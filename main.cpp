8f8faea9d5538f2d4d9e2e0d4cf5b329dd2c62b3
Vikki <vikkibarnwal2026@gmail.com>
2026-06-22 19:18:08 +0545

    Cloned and modified

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-    cout << "Hello, Ross!" << endl <<Vikki here! << endl;
+    cout << "Hello, Ross! ...." << endl <<Vikki here this side! << endl;
     return 0;
 }
